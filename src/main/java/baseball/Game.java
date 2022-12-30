4b914caf332c7907a5eb6e6fa375ee4fff4b7619
sj <slaayer60@gmail.com>
2022-12-30 17:46:04 +0900

    feat: 정답과 입력을 비교하여 결과를 생성한다

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,22 +4,82 @@ import java.util.Random;
 
 public class Game {
 
-    private String number;
-
-    public void start(){
-        number = generateNumber();
+    public void start() {
+        String number = generateNumber();
         System.out.println(number);
     }
 
-    private String generateNumber(){
+    private String generateNumber() {
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<3;i++){
-            sb.append(createRandomInt(1,9));
+        for (int i = 0; i < 3; i++) {
+            sb.append(createRandomInt(1, 9));
         }
         return sb.toString();
     }
 
-    private int createRandomInt(int min, int max){
-        return (int)(Math.random()*(max-min+1) + min);
+    private int createRandomInt(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+
+    private Judgment judge(String input, String answer) {
+        Judgment judgment = new Judgment(answer);
+
+        for(int i=0;i<3;i++){
+            judgment.pitch(i, input.charAt(i));
+        }
+
+        return judgment;
+    }
+
+    static class Judgment {
+        private final String answer;
+        private int strike;
+        private int ball;
+
+        public Judgment(String answer) {
+            this.answer = answer;
+            this.strike = 0;
+            this.ball = 0;
+        }
+
+        public void pitch(int index, char num){
+            if(isStrike(index,num)){
+                strike++;
+                return;
+            }
+
+            if(answer.contains(num + "")){
+                ball++;
+                return;
+            }
+        }
+
+        private boolean isStrike(int index, char num) {
+            return answer.charAt(index) == num;
+        }
+
+        private boolean isBall(char num){
+            return answer.contains(num + "");
+        }
+
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+
+            if (ball > 0) {
+                sb.append(ball).append("볼 ");
+            }
+
+            if (strike > 0) {
+                sb.append(strike).append("스트라이크");
+            }
+
+            if (ball == 0 && strike == 0) {
+                sb.append("낫싱");
+            }
+
+            return sb.toString();
+        }
     }
 }
